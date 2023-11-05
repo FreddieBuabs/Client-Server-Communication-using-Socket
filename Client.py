76f848ec91b509823deccae0e363376d82066273
FreddieBuabs <fbuaba11@gmail.com>
2023-11-05 23:18:50 +0000

    Updated.

--- Client.py
+++ Client.py
@@ -21,7 +21,7 @@ def send(msg):
 
 Message = input("Enter a message you wish to send to server: ")
 send(Message)
-Message2 = input("Enter any other message you wish to send to server")
+Message2 = input("Enter any other message you wish to send to server: ")
 send(Message2)
 
 send(DISCONNECT_MESSAGE)
